f3c01bfc5031166e668e4e0c70c556c25613bf44
chandreshD4 <rajputchandresh163pc1@gmail.com>
2026-07-22 11:53:35 +0530

    Update main.dart to prevent crash

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,19 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text("KC Downloader")),
-        body: const Center(child: Text("KC Downloader Ready!")),
+        body: const Center(
+          child: Text(
+            "App is working perfectly!",
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
       ),
     );
   }
